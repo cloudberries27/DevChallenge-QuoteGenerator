2afdea0daf557872fd18baac12149adb68a48b58
cloudberries27 <ccrod2298@gmail.com>
2022-06-16 16:56:06 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = React.useState("")
+  const [author, setAuthor] = React.useState("")
+  const [genre, setGenre] = React.useState("")
+  const [quotes, setQuotes] = React.useState([])
+  const [singleQuote, setSingleQuote] = React.useState(true)
+  React.useEffect(()=>{
+    getRandomQuote();
+  }, [])
+
+  const getRandomQuote = async () =>{
+    await fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+    .then(response => response.json())
+    .then(result => {
+      setQuote(result.data[0].quoteText)
+      setAuthor(result.data[0].quoteAuthor)
+      setGenre(result.data[0].quoteGenre)
+    });
+  }
+
+  const getAllQuotes = async () => {
+    await fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${author}`)
+    .then(response => response.json())
+    .then(result => {
+      setQuotes(result.data)
+      setSingleQuote(false)
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandomQuote}>Random</button>
+      { singleQuote && (
+        <div>
+          <p>{quote}</p>
+          <div onClick={getAllQuotes}>
+            <p>{author}</p>
+            <p>{genre}</p>
+          </div>
+        </div>
+      )}
+      { !singleQuote && (
+        <div>
+          <p>{author}</p>
+          {quotes.map((word, key)=>{
+            return (<div key={key}> {word.quoteText} </div>)
+          })}
+          <button onClick={()=>setSingleQuote(true)}>Return</button>
+        </div>
+      )}
+      
     </div>
   );
 }
